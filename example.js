e8042b37b4e67787dc7929c8e67c99c64415cdc7
Kenneth Kebaara <kkebaara@yahoo.com>
2022-01-24 18:00:09 -0700

    commit tree 301fcf9f319602677ddd65464c292fa3810b639e

---
+++ example.js
@@ -0,0 +1,26 @@
+const formData = {
+    dogName: "Byron",
+    dogBreed: "Poodle",
+  };
+  
+  const configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(formData),
+  };
+  
+  fetch("http://localhost:3000/users", configurationObject)
+  .then(function (response) {
+      return response.json();
+    })
+    .then(function (object) {
+      console.log(object);
+    })
+    .catch(function (error) {
+      alert('Unauthorized Access');
+      console.log(error.message);
+    });
+
--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Add your code here
+
+// submitData()
+// 1) makes a POST request to /users with a name and email
+// 2) handles the POST request response, retrieves the new id value and appends it to the DOM
+// 3) handles a failed POST request using catch, appends the error message to the DOM
+
+const usersUrl = "http://localhost:3000/users"
+
+const data = {
+  name: "Steve",
+  email: "steve@steve.com",
+};
+
+function submitData(){
+  const configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(data),
+  };
+
+  fetch(usersUrl, configurationObject)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(object) {
+      console.log(object)
+    })
+    .catch(function (error) {
+      alert('Unauthorized Access');
+      console.log(error.message);
+    });
+}
+submitData()
